33ad8ea31343befaf9a04e6e4076fc7be28ec9ae
php2-department <52025621+php2-department@users.noreply.github.com>
2019-07-08 12:47:20 +0530

    Add files via upload

---
+++ rest_api/p1/dbcon1.php
@@ -0,0 +1,6 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "partner1") or die(mysqli_error());
+
+
+?>
---
+++ rest_api/p2/dbcon2.php
@@ -0,0 +1,6 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "partner2")or die(mysqli_error());
+
+
+?>
